c7d3640863608cd55e33831eecf684b324c335fe
omenkish <omenkish@gmail.com>
2020-02-27 13:44:36 +0100

    Implemented cloudinary for image upload

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,5 +1,25 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
-  storage :file
+  include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
+
+  # Specify dimensions for images to resize to for appropriate display fit
+  version :standard do
+    process resize_to_fill: [400, 400, :north]
+  end
+
+  version :thumbnail do
+    resize_to_fit(50, 50)
+  end
+
+  # Cloudinary will be utilized in production (Heroku) while local psql stored us utilized in development and testing environment.
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+    CarrierWave.configure do |config|
+      config.cache_storage = :file
+    end
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
