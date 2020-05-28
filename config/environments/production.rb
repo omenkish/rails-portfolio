f0b5745ed1c742715d30e4819d522a1e9f59bf96
Omenkish <omenkish@gmail.com>
2020-05-28 11:35:16 +0100

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://eneojo-portfolio.herokuapp.com', 'http://eneojo-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://eneojo-portfolio.herokuapp.com/cable"
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
     end
   end
 
-  mount ActionCable.server => '/server'
+  mount ActionCable.server => '/cable'
 end
